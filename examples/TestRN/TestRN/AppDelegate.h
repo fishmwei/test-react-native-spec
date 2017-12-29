e68543d47081b030c3c1880dbf8eb946eaf87bad
huangmingwei <huangmingwei88@qq.com>
2017-12-29 10:55:01 +0800

    Refactor : 创建统一的工程

---
+++ examples/TestRN/TestRN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRN
+//
+//  Created by huangmingwei on 2017/12/29.
+//  Copyright © 2017年 huangmingwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ examples/TestRN/TestRN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRN
+//
+//  Created by huangmingwei on 2017/12/29.
+//  Copyright © 2017年 huangmingwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
