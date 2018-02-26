a2ccdb08fe134a852d5d36b1555015857f20876e
huangmingwei <huangmingwei88@qq.com>
2018-02-26 11:09:21 +0800

    RN 53 crash

--- examples/TestRN53/TestRN53/ViewController.m
+++ examples/TestRN53/TestRN53/ViewController.m
@@ -1,14 +1,19 @@
 //
 //  ViewController.m
-//  TestRN53
+//  TestRN493
 //
-//  Created by huangmingwei on 2018/2/9.
-//  Copyright © 2018年 huangmingwei. All rights reserved.
+//  Created by huangmingwei on 2017/11/15.
+//  Copyright © 2017年 huangmingwei. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
-@interface ViewController ()
+
+@interface ViewController (){
+    RCTRootView *_rootView;
+}
 
 @end
 
@@ -16,9 +21,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self loadReactView];
+    
+    
 }
 
+
+- (void)loadReactView {
+    
+    
+    NSURL *jsCodeLocation;
+    
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle" subdirectory:@"bundle"];
+    if (!jsCodeLocation) {
+        jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+
+    }
+    
+    _rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                            moduleName:@"RN53"
+                                     initialProperties:nil
+                                         launchOptions:nil];
+    _rootView.backgroundColor = [UIColor lightGrayColor];
+    
+    _rootView.frame = CGRectMake(0, 0, 300, 400);
+    
+    [self.view addSubview:_rootView];
+    
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn addTarget:self action:@selector(reloadView) forControlEvents:UIControlEventTouchUpInside];
+    btn.frame = CGRectMake(0, 410, 300, 44);
+    [self.view addSubview:btn];
+    [btn setTitle:@"reload many times" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [btn setBackgroundColor:[UIColor greenColor]];
+    
+    
+}
+
+- (void)reloadView {
+    for (int i = 0; i < 10000; i++) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [_rootView.bridge reload];
+        });
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -27,3 +77,4 @@
 
 
 @end
+
