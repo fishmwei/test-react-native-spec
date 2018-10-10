9dadd837658d2d3ef37ff1886cd3974afc951224
huangmingwei <104800@nd.mail.101.com>
2018-10-10 15:17:31 +0800

    # Type(<scope>): <subject>
    # <body>
    
    # <footer>

---
+++ examples/TestYogac11/TestYogac11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestYogac11
+//
+//  Created by huangmingwei on 2018/3/27.
+//  Copyright © 2018年 huangmingwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ examples/TestYogac11/TestYogac11/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestYogac11
+//
+//  Created by huangmingwei on 2018/3/27.
+//  Copyright © 2018年 huangmingwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
